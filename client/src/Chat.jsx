2050d7786084340ac40132171cb76edd269be719
Shams Misbah <mailshamsmisbah@gmail.com>
2024-03-26 00:59:32 +0530

    Update Chat.jsx

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -19,7 +19,7 @@ export default function Chat() {
     connectToWs();
   }, [selectedUserId]);
   function connectToWs() {
-    const ws = new WebSocket('ws://mern-chat-api-theta.vercel.app:4040');
+    const ws = new WebSocket('ws://mern-chat-api-theta.vercel.app');
     setWs(ws);
     ws.addEventListener('message', handleMessage);
     ws.addEventListener('close', () => {
